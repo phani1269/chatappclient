0f0d809692620d766c1991ecc00e39573e279e5c
PhaniKumar <veldurph@nexturn.com>
2023-06-25 12:49:11 +0530

    signup added

--- src/app/components/signup/signup.component.ts
+++ src/app/components/signup/signup.component.ts
@@ -1,4 +1,27 @@
 import { Component } from '@angular/core';
+import { ValidatorFn, AbstractControl, ValidationErrors, FormGroup, FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { HotToastService } from '@ngneat/hot-toast';
+import { switchMap } from 'rxjs';
+import { AuthenticationService } from 'src/app/_services/auth/authentication.service';
+import { UsersService } from 'src/app/_services/user/users.service';
+
+
+export function passwordsMatchValidator(): ValidatorFn {
+  return (control: AbstractControl): ValidationErrors | null => {
+    const password = control.get('password')?.value;
+    const confirmPassword = control.get('confirmPassword')?.value;
+
+    if (password && confirmPassword && password !== confirmPassword) {
+      return {
+        passwordsDontMatch: true,
+      };
+    }
+
+    return null;
+  };
+}
+
 
 @Component({
   selector: 'app-signup',
@@ -6,5 +29,59 @@ import { Component } from '@angular/core';
   styleUrls: ['./signup.component.scss']
 })
 export class SignupComponent {
+  signUpForm = new FormGroup(
+    {
+      name: new FormControl('', Validators.required),
+      email: new FormControl('', [Validators.email, Validators.required]),
+      password: new FormControl('', Validators.required),
+      confirmPassword: new FormControl('', Validators.required),
+    },
+    { validators: passwordsMatchValidator() }
+  );
 
+  constructor(
+    private authService: AuthenticationService,
+    private toast: HotToastService,
+    private router: Router,
+    private usersService: UsersService
+  ) {}
+
+  ngOnInit(): void {}
+
+  get name() {
+    return this.signUpForm.get('name');
+  }
+
+  get email() {
+    return this.signUpForm.get('email');
+  }
+
+  get password() {
+    return this.signUpForm.get('password');
+  }
+
+  get confirmPassword() {
+    return this.signUpForm.get('confirmPassword');
+  }
+
+  submit() {
+    if (!this.signUpForm.valid) return;
+
+    const { name, email, password } = this.signUpForm.value;
+    this.authService
+      .signUp(email, password)
+      .pipe(
+        switchMap(({ user: { uid } }) =>
+          this.usersService.addUser({ uid, email, displayName: name })
+        ),
+        this.toast.observe({
+          success: 'Congrats! You are all signed up',
+          loading: 'Signing in',
+          error: ({ message }) => `${message}`,
+        })
+      )
+      .subscribe(() => {
+        this.router.navigate(['/home']);
+      });
+  }
 }
